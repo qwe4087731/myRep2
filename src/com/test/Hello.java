ae24a79ab6dccaab23ddc534c65b1dfbde07ff64
qwe4087731 <qwe4087731@gmail.com>
2014-07-06 18:09:57 +0800

    Signed-off-by: qwe4087731 <qwe4087731@gmail.com>

---
+++ src/com/test/Hello.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println(123);
+		System.out.println(45);
+		System.out.println(345);   
+		System.out.println("234");
+	}
+}
